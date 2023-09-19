30ee647747847e939e76dffe0ab018d5949270b9
NURUL EKA MARYANA <110007326+Nurul-ekamaryana@users.noreply.github.com>
2023-09-19 11:20:53 +0700

    first commit

--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -129,12 +129,12 @@
   </div>
   <!-- /.content-wrapper -->
 
-  <footer class="main-footer">
+  {{-- <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
     <strong>Copyright &copy; 2023 By: Kartika Sari Dewi</a>.</strong> All rights reserved.
-  </footer>
+  </footer> --}}
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
